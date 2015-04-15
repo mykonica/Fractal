2dd756a95547fd11d1bbb6f5cb2d741e76f7cc53
betahuang-notebook <betahuang@tencent.com>
2015-04-15 23:42:42 +0800

    给mandelbrot着色

--- Mandelbrot/js/init.js
+++ Mandelbrot/js/init.js
@@ -3,9 +3,35 @@ function clearCanvas() {
 }
 
 $(document).ready(function() {
-	window.MANDELBROT_STEPS = 50;
+	// $('#steps')[0].value = '50';
+
+	window.MANDELBROT_STEPS = parseInt($('#steps')[0].value);
+	window.colors = buildColorArray(window.MANDELBROT_STEPS);
+
 	window.canvas = $('#paint-canvas')[0];
 	window.context = window.canvas.getContext('2d');
+	$('#use_color')[0].checked = true;
+	paintMandelbrot(window.context, window.canvas.width, window.canvas.height, parseInt($('#steps')[0].value), $('#use_color')[0].checked, ('#show_axis')[0].checked);
 
-	paintMandelbrot();
+
+	$('#paint').click(function() {
+		var curSteps = parseInt($('#steps')[0].value);
+		if (curSteps != window.MANDELBROT_STEPS) {
+			window.MANDELBROT_STEPS = curSteps;
+			window.colors = buildColorArray(window.MANDELBROT_STEPS);
+		}
+
+		clearCanvas();
+		paintMandelbrot(window.context, window.canvas.width, window.canvas.height, window.MANDELBROT_STEPS, $('#use_color')[0].checked, $('#show_axis')[0].checked);
+	});
+
+	$('#use_color').click(function() {
+		clearCanvas();
+		paintMandelbrot(window.context, window.canvas.width, window.canvas.height, window.MANDELBROT_STEPS, $('#use_color')[0].checked, $('#show_axis')[0].checked);
+	});
+
+	$('#show_axis').click(function() {
+		clearCanvas();
+		paintMandelbrot(window.context, window.canvas.width, window.canvas.height, window.MANDELBROT_STEPS, $('#use_color')[0].checked, $('#show_axis')[0].checked);
+	});
 });
--- Mandelbrot/js/mandelbrot.js
+++ Mandelbrot/js/mandelbrot.js
@@ -6,49 +6,76 @@ function randomColor() {
 	return 'rgb(' + r + ',' + g + ',' + b + ')';
 }
 
-function ismandelbrot(re, im) {
+function buildColorArray(size) {
+	var colors = new Array();
+	for (var i = 0; i < size; i++) {
+		colors[i] = randomColor();
+	}
+
+	return colors;
+}
+
+function mandelbrotStep(re, im, steps) {
 	var x = 0.0;
 	var y = 0.0;
 	var nx = 0.0;
 	var ny = 0.0;
 
-	for (var i = 0; i < window.MANDELBROT_STEPS; i++) {
+	for (var i = 0; i < steps; i++) {
 		nx = x * x - y * y + re;
 		ny = 2 * x * y + im;
 
 		if (nx * nx + ny * ny > 4) {
-			return false;
+			return i;
 		}
 
 		x = nx;
 		y = ny;
 	}
 
-	return true;
+	return steps;
 }
 
-function paintMandelbrot() {
-	window.context.save();
 
-	var width = window.canvas.width;
-	var height = window.canvas.height;
-	
+
+function paintMandelbrot(context, width, height, steps, colored, axis) {
+	context.save();
+
 	var re = 0.0;
 	var im = 0.0;
-	var scale_factor = 2.0 / height;
+	var scale_factor = 4.0 / height;
 
-	window.context.fillStyle = '#000000';
+	context.fillStyle = '#000000';
 
 	for (var i = 0; i < width; i++) {
 		for (var j = 0; j < height; j++) {
 			re = i * scale_factor - 2.0;
-			im = j * scale_factor - 1.0;
+			im = j * scale_factor - 2.0;
 
-			if (ismandelbrot(re, im)) {
-				window.context.fillRect(i, j, 1, 1);			
+			var step = mandelbrotStep(re, im, steps);
+			if (colored) {
+				if (step == steps) {
+					context.fillStyle = '#000000';
+				} else {
+					context.fillStyle = window.colors[step];
+				}
+				window.context.fillRect(i, j, 1, 1);	
+			} else {
+				if (step == steps) {
+					context.fillRect(i, j, 1, 1);			
+				}				
 			}
 		}
 	}
 
-	window.context.restore();
+ 	if (axis) {
+ 		context.strokeStyle = '#FF0000';
+ 		context.moveTo(0, height / 2);
+ 		context.lineTo(width, height / 2);
+ 		context.moveTo(width / 2, 0);
+ 		context.lineTo(width / 2, height);
+ 		context.stroke();
+ 	}
+
+	context.restore();
 }
