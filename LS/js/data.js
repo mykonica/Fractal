201f4796ee9be6d94c2ecf40d4b5f89baa5c38fd
betahuang <betahuang@tencent.com>
2015-05-05 19:16:43 +0800

    加入LS文法

---
+++ LS/js/data.js
@@ -0,0 +1,14 @@
+ls_table = {
+	'table':[
+		{
+			'name':'羊齿叶', 
+			'org':{'x':0, 'y':200},
+			'steps':10,
+			'initial':'F++F++F',
+			'angle':60
+			'rules':[
+				{'source':'F', 'target':'F--F++F--F--F'}
+			]
+		}
+	]
+};
